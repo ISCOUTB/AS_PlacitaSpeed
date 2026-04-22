3d14bd5f5389e7fd33bdbf3855d658541cbcf852
dylecker <eckerlopezdylan@gmail.com>
2026-04-21 23:35:51 -0500

    Reorder imports and module properties in UsersModule

--- placita_speed_backend/src/users/users.module.ts
+++ placita_speed_backend/src/users/users.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User])],
-    exports: [TypeOrmModule],
-    providers: [UsersService],
-    controllers: [UsersController]
+  imports: [TypeOrmModule.forFeature([User])], // 
+  controllers: [UsersController],
+  providers: [UsersService],
 })
 export class UsersModule {}
