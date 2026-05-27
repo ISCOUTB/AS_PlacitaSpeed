7ba3841f5dca0eca255cb248d97fa7b6e1a45d31
HasmetG <hasgope@gmail.com>
2026-05-26 22:02:12 -0500

    backend y fronent connect

--- placita_speed_backend/src/main.ts
+++ placita_speed_backend/src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(process.env.PORT ?? 9000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
